2ccb7c3b6f64b78e3770d46a6bc1eb02b7e0d0c8
Littoo <80081180+Littoo@users.noreply.github.com>
2021-04-01 00:05:55 +0800

    Issue connecting with mongodb atlas online

--- app.js
+++ app.js
@@ -9,9 +9,30 @@ const AuthRoute = require('./routes/auth')
 
 require("dotenv/config")
 
+mongoose.connect(
+    process.env.DB_CONNECTION_LOCAL_STRING, 
+    {useUnifiedTopology: true, useNewUrlParser: true}   
+    //,(req,res)=> {
+    //console.log("Connected to the database");}
+)
+
+const db = mongoose.connection
+
+db.on('error', (err)=> {
+    console.log(err)
+})
+
+db.once('open', ()=>{
+    console.log('Database Connection Established')
+})
+
 const app = express()
 
 app.use(express.json())
+
+//app.use(morgan('dev'))
+//app.use(bodyParser.urlencoded({extended: true}))
+//app.use(bodyParser.json())
 
 app.get("/", (req, res)=>{
     res.send("First request hahahaha !!!!")
@@ -37,25 +58,10 @@ app.post("/create_user", async (req, res) => {
     }
 })
 
-mongoose.connect(
-    process.env.DB_CONNECTION_STRING, 
-    {useUnifiedTopology: true, useNewUrlParser: true}   
-    //,(req,res)=> {
-    //console.log("Connected to the database");}
-)
 
-const db = mongoose.connection
-
-db.on('error', (err)=> {
-    console.log(err)
-})
-
-db.once('open', ()=>{
-    console.log('Database Connection Established')
-})
 
 app.listen(3000, ()=> {
     console.log("Listening to 3000");
 })
 
-app.use('/api', AuthRoute)
+app.use('/user', AuthRoute)
