1167a833a961c85b299872b91afe30b4a429cd9b
Littoo <80081180+Littoo@users.noreply.github.com>
2021-03-31 23:20:28 +0800

    Updated app with register route

--- app.js
+++ app.js
@@ -1,45 +1,48 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require("express")
+const mongoose = require("mongoose")
 
-const User = require("./models/user");
+const User      = require("./models/user")
+const AuthRoute = require('./routes/auth')    
 
-require("dotenv/config");
+require("dotenv/config")
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 app.get("/", (req, res)=>{
-    res.send("First request hahahaha !!!!");
-});
+    res.send("First request hahahaha !!!!")
+})
 
 app.get("/users", (req, res)=>{
 
     let users = ["Lito", "Jazzy", "Kasuy", "Tita", "Calooy"]
     res.send({
         users: users,
-    });
-});
+    })
+})
 
 app.post("/create_user", async (req, res) => {
     try{
-        const myuser = new User(req.body);
-        await myuser.save();
-        res.send(myuser);
+        const myuser = new User(req.body)
+        await myuser.save()
+        res.send(myuser)
     }catch(err){
         res.send({
             message:err
-        });
+        })
     }
-});
+})
 
 mongoose.connect(
     process.env.DB_CONNECTION_STRING, 
     {useUnifiedTopology: true, useNewUrlParser: true},    
     (req,res)=> {
     console.log("Connected to the database");
-});
+})
 
 app.listen(3000, ()=> {
     console.log("Listening to 3000");
-});
+})
+
+app.use('/api', AuthRoute)
