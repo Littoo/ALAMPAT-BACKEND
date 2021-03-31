0e2a52bb932c40e8b4ddbcc1eab12fbf700a40dd
Littoo <80081180+Littoo@users.noreply.github.com>
2021-03-31 22:59:05 +0800

    Added controllers

---
+++ controllers/AuthController.js
@@ -0,0 +1,38 @@
+const User      = require('../models/user')
+const bcrypt    = require('bcryptjs')
+const jwt       = require('jsonwebtoken')
+
+const register = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10, function(err, hashedPass){
+        if(err){
+            res.json({
+                error:err
+            })
+        }
+    })
+
+    let user = new User ({
+        name: req.body.name,
+        email: req.body.email,
+        phone_number: req.body.phone_number,
+        address: req.body.address,
+        password: hashedPass
+    })
+
+    user.save()
+    .then(user => {
+        res.json({
+            message: 'User Registered successfully!'
+        })
+    })
+
+    .catch(error =>{
+        res.json({
+            message: 'An error occurred!'
+        })
+    })
+}
+
+module.exports = {
+    register
+}
