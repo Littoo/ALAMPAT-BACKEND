e696cc48f3ba3890541a6a79e1eca713970f9ea7
Littoo <80081180+Littoo@users.noreply.github.com>
2021-04-01 16:50:14 +0800

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -13,8 +13,9 @@ const register = (req, res, next) => {
         let user = new User ({
             name: req.body.name,
             email: req.body.email,
-            phone_number: req.body.phone_number,
+            phoneNumber: req.body.phoneNumber,
             address: req.body.address,
+            userType: req.body.userType,
             password: hashedPass
         })
     
@@ -35,6 +36,39 @@ const register = (req, res, next) => {
    
 }
 
+const login = (req, res, next)  =>{
+    var username = req.body.username
+    var password = req.body.password
+
+    User.findOne({$or: [{email:username}, {phoneNumber:username}]})
+    .then(user => {
+        if(user){
+            bcrypt.compare(password, user.password, function(err, result){
+                if(err){
+                    res.json({
+                        error: err
+                    })
+                }
+                if(result){
+                    let token = jwt.sign({name: user.name}, 'verySecretValue', {expiresIn: '1h'})
+                    res.json({
+                        message: 'Login Successful!',
+                        token
+                    })
+                } else{
+                    res.json({
+                        message: 'Password does not matched!'
+                    })
+                }
+            })
+        }else{
+            res.json({
+                message: 'No user found'
+            })
+        }
+    })
+}
+
 module.exports = {
-    register
+    register, login
 }
